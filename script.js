e6d50018fc4d62932df8ebdc61070def6f74920f
Farid Shaikh <faridshaikh@MacBookPro.phub.net.cable.rogers.com>
2026-01-21 22:05:43 -0500

    created HTML body to have a specific font and link the file to the javascript file

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+
+//array for list of movies 
 const movies = [
   "Iron Man",
   "Captain America",
   "The Avengers"
 ];
 
+//Grab elements from HTML 
 const button = document.getElementById("showBtn");
 const list = document.getElementById("movieList");
 
+
+//Click Event for the button element
 button.addEventListener("click", () => {
+    //Clears list from any duplicates happening by setting to an empty string
   list.innerHTML = "";
-  movies.forEach(movie => {
+  //Create the list item by going through the movies array in the loop
+  movies.forEach(movieItem => {
+    //create list item element
     const li = document.createElement("li");
-    li.textContent = movie;
+    //set the movie item to the text content 
+    li.textContent = movieItem;
+    //add the movieItems to the list 
     list.appendChild(li);
   });
 });
